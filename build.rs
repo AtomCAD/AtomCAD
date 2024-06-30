b763a6f02537d0fb985d15fedee345090a160550
Mark Friedenbach <mark@friedenbach.org>
2024-06-30 01:28:04 -0700

    Embed app icon into executable on Windows.

---
+++ build.rs
@@ -0,0 +1,15 @@
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of
+// the MPL was not distributed with this file, You can obtain one at <http://mozilla.org/MPL/2.0/>.
+
+extern crate embed_resource;
+use std::env;
+
+fn main() {
+    let target = env::var("TARGET").unwrap();
+    if target.contains("windows") {
+        // On Windows we will set our app icon as icon for the executable.
+        embed_resource::compile("build/windows/icon.rc", embed_resource::NONE);
+    }
+}
+
+// End of File
